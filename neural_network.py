15bef8e553c9a0434e21371bbd081166c489a7b7
benpbo <prizeben@gmail.com>
2020-09-07 22:38:12 +0300

    Updated nn

--- neural_network.py
+++ neural_network.py
@@ -32,37 +32,36 @@ class NeuralNetwork:
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
         to ``self.biases`` and ``self.weights``."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]    
-        
-        for x, y in zip(xs, ys):
-            # feedforward
-            activation = x
-            activations = [x]  # list to store all the activations, layer by layer
-            zs = []  # list to store all the z vectors, layer by layer
-            for b, w in zip(self.biases, self.weights):
-                z = np.dot(w, activation)+b
-                zs.append(z)
-                activation = sigmoid(z)
-                activations.append(activation)
-            # backward pass
-            delta = self.cost_derivative(activations[-1], y) * \
-                sigmoid_prime(zs[-1])
-            nabla_b[-1] += delta
-            nabla_w[-1] += np.dot(delta, activations[-2].transpose())
-            # Note that the variable l in the loop below is used a little
-            # differently to the notation in Chapter 2 of the book.  Here,
-            # l = 1 means the last layer of neurons, l = 2 is the
-            # second-last layer, and so on.  It's a renumbering of the
-            # scheme in the book, used here to take advantage of the fact
-            # that Python can use negative indices in lists.
-            for l in range(2, self.num_layers):
-                z = zs[-l]
-                sp = sigmoid_prime(z)
-                delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-                nabla_b[-l] += delta
-                nabla_w[-l] += np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+        nabla_bs = [np.zeros(b.shape) for b in self.biases]
+        nabla_ws = [np.zeros(w.shape) for w in self.weights]
+
+        # feedforward
+        activation = xs
+        activations = [xs]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = (w @ activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # backward pass TODO: vectorize
+        delta = self.cost_derivative(activations[-1], ys) * sigmoid_prime(zs[-1])
+        nabla_bs[-1] = delta[0]
+        nabla_ws[-1] = delta[0] @ activations[-2][0].transpose()
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = (self.weights[-l+1].transpose() @ delta) * sp
+            nabla_bs[-l] += delta[0]
+            nabla_ws[-l] += delta[0] @ activations[-l-1][0].transpose()
+        return nabla_bs, nabla_ws
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
@@ -73,10 +72,9 @@ class NeuralNetwork:
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
         xs, ys = mini_batch
-        delta_nabla_b, delta_nabla_w = self.backprop(xs, ys)
+        delta_nabla_b, delta_nabla_w = self.backprop(list(xs), list(ys))
         nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
         nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-            
 
         self.weights = [
             w-(eta/len(mini_batch))*nw
@@ -127,9 +125,8 @@ class NeuralNetwork:
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
-        return (output_activations-y)
+        """Return the vector of partial derivatives for the output activations."""
+        return output_activations-y
 
 
 # Miscellaneous functions
